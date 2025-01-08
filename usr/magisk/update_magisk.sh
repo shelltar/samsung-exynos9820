3c42b719f52c650baa93423b35ebfcf4cc987b8f
shelltar <45462483+shelltar@users.noreply.github.com>
2025-01-08 17:26:51 +0100

    Update update_magisk.sh

--- usr/magisk/update_magisk.sh
+++ usr/magisk/update_magisk.sh
@@ -8,54 +8,44 @@ ver="$(cat "$DIR/magisk_version" 2>/dev/null || echo -n 'none')"
 
 if [[ "$1" == *"-kitsune" ]]; then
     kitsune_version="${1%-kitsune}"
-    if [[ -z "$kitsune_version" ]]; then
-        echo "Error: Invalid kitsune argument. Expected format: <version>-kitsune"
-        exit 1
-    fi
-
     nver="$(curl -s https://github.com/HuskyDG/magisk-files/releases | grep -B1 "$kitsune_version" | grep -m 1 -Poe '[0-9]{10}')"
-    if [[ -z "$nver" ]]; then
-        echo "Error: Failed to find a release tag for kitsune version: $kitsune_version"
-        exit 1
-    fi
-	
     magisk_link="https://github.com/HuskyDG/magisk-files/releases/download/${nver}/app-release.apk"
-	
+
 elif [[ "$1" =~ ^canary(-[0-9]+)?$ ]]; then
- 	if [[ "$1" == "canary" ]]; then
-		nver="$(curl -s https://github.com/topjohnwu/Magisk/releases | grep -m 1 -Poe 'canary-[0-9]{5}')"
-	else
-		nver="$1"
-	fi
-	magisk_link="https://github.com/topjohnwu/Magisk/releases/download/${nver}/app-release.apk"
- elif [[ "$1" =~ ^v[0-9]+\.[0-9]+(-kitsune-[0-9]+|-([a-f0-9]+))?$ ]]; then
-    if [[ "$1" =~ ^v[0-9]+\.[0-9]+-kitsune-[0-9]+$ ]]; then
-        nver="$1"
-    elif [[ "$1" =~ ^v[0-9]+\.[0-9]+-[a-f0-9]+$ ]]; then
-        nver="$1"
+    if [[ "$1" == "canary" ]]; then
+        nver="$(curl -s https://github.com/topjohnwu/Magisk/releases | grep -m 1 -Poe 'canary-[0-9]{5}')"
     else
-        nver="$(curl -s https://github.com/1q23lyc45/KitsuneMagisk/releases | grep -m 1 -Poe 'v[0-9]+\.[0-9]+-kitsune-[0-9]+')"
+        nver="$1"
     fi
+    magisk_link="https://github.com/topjohnwu/Magisk/releases/download/${nver}/app-release.apk"
+
+elif [[ "$1" =~ ^v[0-9]+\.[0-9]+(-kitsune-[0-9]+|-([a-f0-9]+))?$ ]]; then
+    nver="$1"
     magisk_link="https://github.com/1q23lyc45/KitsuneMagisk/releases/download/${nver}/app-release.apk"
+
 else
-	dash='-'
-	if [ "x$1" = "x" ]; then
-		nver="$(curl -s https://github.com/topjohnwu/Magisk/releases | grep -m 1 -Poe 'Magisk v[\d\.]+' | cut -d ' ' -f 2)"
-	else
-		nver="$1"
-	fi
-	if [ "$nver" = "v26.3" ]; then
-		dash='.'
-	fi
-	magisk_link="https://github.com/topjohnwu/Magisk/releases/download/${nver}/Magisk${dash}${nver}.apk"
+    dash='-'
+    if [[ -z "$1" ]]; then
+        nver="$(curl -s https://github.com/topjohnwu/Magisk/releases | grep -m 1 -Poe 'Magisk v[\d\.]+' | cut -d ' ' -f 2)"
+    else
+        nver="$1"
+    fi
+    if [[ "$nver" == "v26.3" ]]; then
+        dash='.'
+    fi
+    magisk_link="https://github.com/topjohnwu/Magisk/releases/download/${nver}/Magisk${dash}${nver}.apk"
 fi
 
 if [[ -n "$nver" && "$nver" != "$ver" || ! -f "$DIR/magiskinit" || "$nver" =~ ^canary(-[0-9]+)?$ || "$nver" =~ ^v[0-9]+\.[0-9]+-kitsune-[0-9]+$ ]]; then
     echo "Updating Magisk from $ver to $nver"
-    curl -s --output "$DIR/magisk.zip" -L "$magisk_link"
+    if ! curl -s --output "$DIR/magisk.zip" -L "$magisk_link"; then
+        echo "Error: Failed to download Magisk from $magisk_link"
+        exit 1
+    fi
     if fgrep 'Not Found' "$DIR/magisk.zip"; then
         curl -s --output "$DIR/magisk.zip" -L "${magisk_link%.apk}.zip"
     fi
+
     if unzip -o "$DIR/magisk.zip" arm/magiskinit64 -d "$DIR"; then
         mv -f "$DIR/arm/magiskinit64" "$DIR/magiskinit"
         : > "$DIR/magisk32.xz"
